b9f84d3898f88bd625545bd7ce9fdec8c06f6c4e
David <krylovdavid14@gmail.com>
2024-08-03 14:44:36 +0500

    Raqamlar qatorining yig‘indisini hisoblash

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int N = 0;
+while (N <= 0)
+{
+    Console.WriteLine("Musbat son kiriting : ");
+    string nStr = Console.ReadLine();
+    N = Convert.ToInt32(nStr);
+}
+
+int yigindi = 0;
+for(int i = 1; i <= N; i++)
+{
+    yigindi += i;
+}
+
+Console.WriteLine($" Output : {yigindi}");
