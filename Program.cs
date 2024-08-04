e0f10109babd262e8b2f486cc1346919511fb878
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-08-04 14:47:48 +0500

    Paritet toq juftlik tekshiruvi

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write(" Input = ");
+string nStr = Console.ReadLine();
+int N = Convert.ToInt32(nStr);
+
+if (N%2 == 0){
+    Console.WriteLine("Output = \"JUFT\" ");
+}else{
+    Console.WriteLine("Output = \"TOQ\" ");
+}
+
