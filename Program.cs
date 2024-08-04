53e9d496dc5292666776703cea0907056389b578
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-08-04 11:55:37 +0500

    Raqamlar-qatorining-yig‘indisini-hisoblash

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int N = 0;
+while (N <= 0){
+    System.Console.WriteLine(" Musbat son kiriting : ");
+    string nStr = Console.ReadLine();
+    N = Convert.ToInt32(nStr);
+}
+
+int yigindi = 0;
+for (int i = 0; i <= N; i++){
+    yigindi += i;
+}
+
+System.Console.WriteLine($" Output : {yigindi}");
